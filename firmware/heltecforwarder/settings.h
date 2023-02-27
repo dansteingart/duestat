c96c45baa32d525a5af379a2d23a9dcd9c4272d1
Dan Steingart <dan@pithy.io>
2023-02-27 07:39:47 -0500

    refactor dirs; add eagle

---
+++ firmware/heltecforwarder/settings.h
@@ -0,0 +1,6 @@
+const char* ssid = "New Port";
+const char* password = "2305bonar";
+const char* mqtt_server = "data.ceec.echem.io";
+const char* mqtt_send = "steingart_lab/data/dan/esp32_bridge_test_duestat/data";
+const char* mqtt_recv = "esp32_bridge_test_duestat/recv";
+const char* mqtt_name = "fooer_doer";
